6d55e0aa1f3e6be56638d472160b11e2777113d1
valdir-alves3000 <valdiralves3000@gmail.com>
2024-06-07 09:30:39 -0300

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import textwrap
+
+def menu():
+  menu = """\n
+  [d]\tDepositar
+  [s]\tSacar
+  [e]\tExtrato
+  [q]\tSair
+  => """
+  return input(textwrap.dedent(menu))
+
+def main():
+  saldo = 0
+  limite = 500
+  extrato = ""
+  numero_saques = 0
+  LIMITE_SAQUES = 3
+
+  while True:
+    opcao = menu()
+
+    if opcao == "d":
+      valor = float(input("Informe o valor do depósito: "))
+
+      if(valor > 0):
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+      else:
+        print("Operação falhou! O valor informado é inválido.")
+    
+    elif opcao == "s":
+      valor = float(input("Informe o valor do saque: "))
+
+      excedeu_saldo = valor > saldo
+      excedeu_limite = valor > limite
+      excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+      if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+      elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede seu limite.")
+      elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+
+      elif valor > 0:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+      else:
+        print("Operação falhou! O valor informado é inválido.")
+      
+    elif opcao == "e":
+      print("\n============= EXTRATO =============")
+      print("Não foi Realizada Movimentação." if not extrato else extrato)
+      print(f"\nSaldo: R$ {saldo:.2f}")
+      print("===================================")
+
+    elif opcao == "q":
+      break
+
+main()
